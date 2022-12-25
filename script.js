ec426c02c2fe2f74ac5593aeffd3011bb1d29cb6
iplaureta <121290891+iplaureta@users.noreply.github.com>
2022-12-25 11:26:32 +1100

    Update script.js

--- script.js
+++ script.js
@@ -3,15 +3,15 @@ const shuffle = document.getElementById("shuffle");
 
 // you can change the questions in here
 const questions = [
-  { id: "q1", content: "quiz1" },
-  { id: "q2", content: "quiz2" },
-  { id: "q3", content: "quiz3" },
-  { id: "q4", content: "quiz4" },
-  { id: "q5", content: "quiz5" },
-  { id: "q6", content: "quiz6" },
-  { id: "q7", content: "quiz7" },
-  { id: "q8", content: "quiz8" },
-  { id: "q9", content: "quiz9" },
+  { id: "q1", content: "test1" },
+  { id: "q2", content: "test2" },
+  { id: "q3", content: "test3" },
+  { id: "q4", content: "test4" },
+  { id: "q5", content: "test5" },
+  { id: "q6", content: "test6" },
+  { id: "q7", content: "test7" },
+  { id: "q8", content: "test8" },
+  { id: "q9", content: "test9" },
 ];
 
 const randomNumber = (arrayLength) => {
